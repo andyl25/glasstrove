707101977aaf650674262dd728d323b0ef966f8e
Andy <21andyslee@gmail.com>
2021-11-27 16:36:16 -0800

    cool pics

--- web/partials/HeroHome.js
+++ web/partials/HeroHome.js
@@ -50,43 +50,23 @@ function HeroHome() {
           {/* Hero image */}
           <div>
             <div className="relative flex justify-center mb-8" data-aos="zoom-y-out" data-aos-delay="450">
-              <div className="flex flex-col justify-center">
-                <img className="mx-auto" src='/images/hero-image.png' width="768" height="432" alt="Hero" />
-                <svg className="absolute inset-0 max-w-full mx-auto md:max-w-none h-auto" width="768" height="432" viewBox="0 0 768 432" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-                  <defs>
-                    <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="hero-ill-a">
-                      <stop stopColor="#FFF" offset="0%" />
-                      <stop stopColor="#EAEAEA" offset="77.402%" />
-                      <stop stopColor="#DFDFDF" offset="100%" />
-                    </linearGradient>
-                    <linearGradient x1="50%" y1="0%" x2="50%" y2="99.24%" id="hero-ill-b">
-                      <stop stopColor="#FFF" offset="0%" />
-                      <stop stopColor="#EAEAEA" offset="48.57%" />
-                      <stop stopColor="#DFDFDF" stopOpacity="0" offset="100%" />
-                    </linearGradient>
-                    <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="hero-ill-e">
-                      <stop stopColor="#4FD1C5" offset="0%" />
-                      <stop stopColor="#81E6D9" offset="25.871%" />
-                      <stop stopColor="#338CF5" offset="100%" />
-                    </radialGradient>
-                    <circle id="hero-ill-d" cx="384" cy="216" r="64" />
-                  </defs>
-                  <g fill="none" fillRule="evenodd">
-                    <circle fillOpacity=".04" fill="url(#hero-ill-a)" cx="384" cy="216" r="128" />
-                    <circle fillOpacity=".16" fill="url(#hero-ill-b)" cx="384" cy="216" r="96" />
-                    <g fillRule="nonzero">
-                      <use fill="#000" xlinkHref="#hero-ill-d" />
-                      <use fill="url(#hero-ill-e)" xlinkHref="#hero-ill-d" />
-                    </g>
-                  </g>
-                </svg>
+              <div className="flex flex-row justify-center space-x-0">
+
+                <img className="rounded-2xl w-1/3 transform skew-y-6" src="https://lh3.googleusercontent.com/GS1ISv04SMCaNyBnc2WAWdQG0MLQ3mkboDqhdEFR8M6PXs6xAC-zUQaMTDk9sfk4OeLIYpA7jqDFChJ8aHXz3nQnNf6drrqS28xL" />
+                <img className="rounded-2xl w-1/3 transform skew-y-6" src="https://lh3.googleusercontent.com/qxky4Bie8peNjK4VkIUyXjXp91tv8a5qfEfP50NOP4pdW4fggUmoJFnII08WYwPYe8QpgJkoXvBzriUN_qTSuxX8VyTXozWqwvi5_g" />
+                
+                <img className="rounded-2xl w-1/3 transform skew-y-6" src="https://lh3.googleusercontent.com/Gwk0ZXD4fs7NomQ7K7zzlF_lORkIwdJCgUMxMk7Ad23wl4x5sKIIVcNOTn5IwUwwJQCsjF9-sa-0NA_bdEWl0a0pxVf2fGXs0twi78o=w404" />
+
               </div>
+              {/* <div className="flex flex-row justify-center space-x-5">
+
+                <img className="rounded-2xl w-1/2" src="https://lh3.googleusercontent.com/GS1ISv04SMCaNyBnc2WAWdQG0MLQ3mkboDqhdEFR8M6PXs6xAC-zUQaMTDk9sfk4OeLIYpA7jqDFChJ8aHXz3nQnNf6drrqS28xL" />
+                <img className="rounded-2xl w-1/2" src="https://lh3.googleusercontent.com/qxky4Bie8peNjK4VkIUyXjXp91tv8a5qfEfP50NOP4pdW4fggUmoJFnII08WYwPYe8QpgJkoXvBzriUN_qTSuxX8VyTXozWqwvi5_g" />
+
+              </div> */}
               <button className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg" onClick={(e) => { e.preventDefault(); e.stopPropagation(); setVideoModalOpen(true); }} aria-controls="modal">
-                <svg className="w-6 h-6 fill-current text-gray-400 group-hover:text-blue-600 flex-shrink-0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
-                  <path d="M10 17l6-5-6-5z" />
-                </svg>
-                <span className="ml-3">Watch the full video (2 min)</span>
+
+                <span>Watch the full video (2 min)</span>
               </button>
             </div>
 
