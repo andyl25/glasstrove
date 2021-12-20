0e5c95e1502da769dd60aabb7099c81555bc2bdd
srinaths32 <srinathsomasundaram32@gmail.com>
2021-12-20 14:57:33 -0800

    server typo

--- server/server/urls.py
+++ server/server/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
