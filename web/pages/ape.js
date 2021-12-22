1cd93584b7964ee6ca6b1787f7e803689d4063f0
Andy <21andyslee@gmail.com>
2021-12-21 22:45:37 -0800

    added bio with mutations

--- web/pages/ape.js
+++ web/pages/ape.js
@@ -1,4 +1,12 @@
 import { gql, useQuery } from "@apollo/client";
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+import Header from "../partials/Header";
+import "tailwindcss/tailwind.css";
+
+// var Web3 = require('web3');
+var web3 = new Web3(Web3.givenProvider);
+
 
 const EXAMPLE = gql`
   query ExampleQuery {
@@ -9,10 +17,50 @@ const EXAMPLE = gql`
   }
 `;
 
-const Ape = () => {
-  const {data, loading, error} = useQuery(EXAMPLE);
+const NONCE = gql`
+  query{
+    me{
+     nonce
+   }}
+`;
 
-  return <div>{console.log(data)}</div>;
+// handleSignMessage = ({ publicAddress, nonce }) => {
+//   return new Promise((resolve, reject) =>
+//     web3.personal.sign(
+//       web3.fromUtf8(`${nonce}`),
+//       publicAddress,
+//       (err, signature) => {
+//         if (err) return reject(err);
+//         return resolve({ publicAddress, signature });
+//       }
+//     )
+//   );
+// };
+
+// handleAuthenticate = ({ publicAddress, signature }) =>
+//     fetch(`${process.env.REACT_APP_BACKEND_URL}/auth`, {
+//       body: JSON.stringify({ publicAddress, signature }),
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       method: 'POST'
+//     }).then(response => response.json());
+
+const Ape = () => {
+  const {data, loading, error} = useQuery(NONCE);
+  useEffect(() => {
+    if(!loading && !error) {
+      return new Promise((resolve, reject) => web3.eth.personal.sign(
+        web3.utils.fromUtf8(data.me.nonce),
+        '0x6551a57CF40b3e0206B9b3D044f2b2830Ff83b17',
+        (err, signature) => {
+          if (err) return reject(err);
+          return resolve({ publicAddress, signature });
+        }
+        ));
+    }
+  })
+  return <div>{!loading && !error && (<div>{data.me.nonce}</div>)}</div>
 };
 
 console.log("hellow")
